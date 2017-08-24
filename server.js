8ef81c175b394bc4818d477734c464d7dd817207
cdore00 <cdore00@yahoo.ca>
2017-08-23 20:44:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -411,7 +411,8 @@ else
 
 var coll = dBase.collection('score');
 coll.find({_id:o_id}).toArray(function(err, doc) {
-doc[0].score_date = tl.getDateTime(doc[0].score_date);
+if (doc[0].score_date != null)
+	doc[0].score_date = tl.etDateTime(doc[0].score_date);
 getBloc(res, doc)
   });
 }
